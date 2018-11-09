af6b88c62f40ccde4a3f4fe1bbf0f6dbecafd508
pedromaASIR <44395287+pedromaASIR@users.noreply.github.com>
2018-11-09 14:28:41 +0100

    Update notasformu.pl

--- notasformu.pl
+++ notasformu.pl
@@ -1,35 +1,42 @@
 #!/usr/bin/perl
 print "Content-Type: text/html\n\n";
 if($ENV{'QUERY_STRING'} eq "") {
-   print "<h1>Dime tu nombre</h1>
-          <form name=\"search\">";
+   print "<h1>Dime tu nombre</h1> <form name=\"search\">";
           print "<input type=\"text\" name=\"nombre\">";
           print "<input type=\"submit\" value=\"Enviar Nombre\" />";
 }
 else
 {
-    @nombre=split("=",$ENV{'QUERY_STRING'});
-    $nom=$nombre[1];
-    print "el nombre es: $nom";
-    print "<h3>Dime el prefijo de la asignatura</h3>
-    <form name=\"search\">";
-    print "<input type=\"text\" name=\"asignatura\">";
-    print "<input type=\"submit\" value=\"Enviar asignatura\"/>";
-}
+    @ar=split("=",$ENV{'QUERY_STRING'});
+    if ($ar[0] eq 'nombre') {
+       $profesor=$ar[1];
+      # -------------------------------------------------------
+      # Aqui averiguo cual es el prefijo que me viene, para 
+      # sabers i es un nombre o una asignatura
+      # presento como respuesta el formulario para que me 
+      # introduzca una asignatura
+      # -------------------------------------------------------
+       print "<h3>Hola $profesor dime el prefijo de la asignatura</h3> <form name=\"search\">";
+       print "<input type=\"text\" name=\"asignatura\">";
+       print "<input type=\"submit\" value=\"Enviar asignatura\"/>";
+    }
 
-{
-  @campo1=split("=",$ENV{'QUERY_STRING'});
-  if($campo1[0] eq "asignatura"){
-  #Aqui contesta al formulario para mostrar solo
-  #Cambia esta linea si tu nombre de usuario no es USUARIO
-  open F, "/home/usuario/notas.txt";
-  while(<F>) {
-    chomp;
-    @campos=split(":");
-    if($campo1[1] eq $campos[0])
+    if ($ar[0] eq 'asignatura') {
+      # -------------------------------------------------------
+      # Si el prefijo que me viene es una asignatura
+      # muestro el contenido del fichero siempre que coincida
+      # con la asignatura introducida.
+      # -------------------------------------------------------
+      # ¡¡OJO!! Cambia esta linea si tu nombre de usuario no es USUARIO
+      open F, "/home/usuario/notas.txt";
+      while(<F>) {
+      chomp;
+      @campos=split(":");
+      if($ar[1] eq $campos[0])
       {
-        print "El alumno $campos[2] tiene un $campos[1] en $campos[0] y es elegido por $nom<br>";
+        print "El alumno: $campos[2] tiene un: $campos[1] en: $campos[0] y la busqueda la ha realizado: $profesor<br>";
       }
     }
   }
 }
+
