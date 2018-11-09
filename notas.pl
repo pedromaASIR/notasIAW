5642e7de59f4305597def5eb005322d4d4dfe220
pedromaASIR <44395287+pedromaASIR@users.noreply.github.com>
2018-11-09 14:40:37 +0100

    Update notas.pl

--- notas.pl
+++ notas.pl
@@ -1,12 +1,21 @@
 #!/usr/bin/perl
 print "Content-Type: text/html\n\n";
 if($ENV{'QUERY_STRING'} eq "") {
+# -------------------------------------------------
+# Si el query string que nos viene es vacio, ya 
+# que es la primera que es ejecutado, nos 
+# mostrara este breve formulario
+# -------------------------------------------------
    print "<h1>Hola, bienvenido a este script simple en perl</h1>
           <h3>Dime el prefijo de la asignatura</h3>
           <form name=\"search\">";
           print "<input type=\"text\" name=\"asignatura\">";
           print "<input type=\"submit\" value=\"enviar\" />";
 }else {
+# -------------------------------------------------
+# En caso de que venga ya relleno el formulario,
+# nos mostrara el resultado
+# -------------------------------------------------
 #Esta linea es la que debes modificar por si tu fichero no esta en /home/usuario/notas.txt, lo que hay entre comillas. 
   @campo1=split("=",$ENV{'QUERY_STRING'});
   open T, "/home/usuario/notas.txt";
